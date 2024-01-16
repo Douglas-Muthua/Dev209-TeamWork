c2aa0df9864bdfb4528733c730489afc6d19f56c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-15 16:34:26 -0800

    Add letter swap functionality and toggle element hide

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 let x = 0
 let storage = []
+let section1 = document.getElementById('section1');
+let button = document.getElementById('swapBtn');
 do{
     let firstWord_Question = window.prompt("Input First Word");
-
     let secondWord_Question = window.prompt("Input Second Word");
-
     let thirdWord_Question = window.prompt("Input Third Word");
-
     storage.push(firstWord_Question, secondWord_Question,thirdWord_Question)
-
     x+1
+}while(x =0)
 
-
-
+function swapLetters(oneWord){
+    return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); 
 }
-while(x =0)
+
+function toggleDisplay(){
+    if(section1.style.display == 'block'){
+        section1.style.display = 'none';
+        section2.style.display = 'block';
+    }
+    else{
+        section1.style.display = 'block';
+        section2.style.display = 'none';
+    }
+}
 
 document.getElementById('firstWord').innerHTML = storage[0];
 document.getElementById('SecondWord').innerHTML = storage[1];
 document.getElementById('ThirdWord').innerHTML = storage[2];
+
+document.getElementById('firstWord1').innerHTML = swapLetters(storage[0]);
+document.getElementById('SecondWord1').innerHTML = swapLetters(storage[1]);
+document.getElementById('ThirdWord1').innerHTML = swapLetters(storage[2]);
+
+section1.style.display = 'block';
+button.style.display = 'block';
+
+button.addEventListener("click", function() {
+    toggleDisplay();
+});
